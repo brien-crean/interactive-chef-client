b7a4204bc27fcc2cb10a748ef8aebb94a2d004d0
modus spare <modusspare@moduss-MacBook-Pro.local>
2019-04-06 16:52:54 -0700

    added recipes to flat list, added fontAwesome and added basic styling

--- app/components/RecipeList.js
+++ app/components/RecipeList.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { ActivityIndicator, StyleSheet, TouchableHighlight, Text, View } from 'react-native';
+import { ActivityIndicator, Image, StyleSheet, FlatList, TouchableHighlight, Text, View } from 'react-native';
+import config from '../lib/config';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import Dimensions from 'Dimensions';
 
 class RecipeList extends Component {
 
@@ -7,35 +10,37 @@ class RecipeList extends Component {
     this.props.getRecipes();
   }
 
-  renderRecipes = (recipes) => {
+  renderRecipe = ({ item }) => {
     const { navigate } = this.props.navigation;
     return (
-      <View>
-        {
-          recipes.map((recipe) => {
-            return(
-              <TouchableHighlight
-                key={recipe.id}
-                onPress={() => navigate('RecipeDetailScreen', { recipe: recipe })}
-                underlayColor='#99d9f4'
-              >
-                <Text>{recipe.title}</Text>
-              </TouchableHighlight>
-              )
-            })
-          }
-      </View>
+      <TouchableHighlight
+        key={ item.id }
+        onPress={() => navigate('RecipeDetailScreen', { recipe: item })}
+        underlayColor='#99d9f4'
+        style={ styles.recipeButtonContainer }
+      >
+        <View style={ styles.recipeButtonContent }>
+          <Image
+            style={ styles.image }
+            source={{ uri: config.ROOT_URL + item.image.small.url }}
+          />
+          <Text style={ styles.recipeTitle }>{ item.title }</Text>
+          <Icon name="chevron-right" size={17} color="grey" />
+        </View>
+      </TouchableHighlight>
     );
   }
 
   render() {
     const { recipes, recipesLoading } = this.props;
     return (
-      <View style={styles.mainContainer}>
-        <Text style={styles.title}>Recipes</Text>
+      <View style={ styles.mainContainer }>
+        <Text style={ styles.title }>Recipes</Text>
         <View>
           {
-            recipesLoading ? <ActivityIndicator size="large" color="0000ff" /> : this.renderRecipes(recipes)
+            recipesLoading
+              ? <ActivityIndicator size="large" color="0000ff" />
+              : <FlatList data={ recipes } renderItem={ this.renderRecipe } />
           }
         </View>
       </View>
@@ -51,6 +56,26 @@ const styles = StyleSheet.create({
     fontSize: 30,
     alignSelf: 'center',
     fontFamily: 'Avenir',
+  },
+  recipeButtonContainer: {
+    width: Dimensions.width,
+    height: 50,
+    justifyContent: 'center',
+    backgroundColor: 'white',
+  },
+  recipeButtonContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: Dimensions.width,
+  },
+  image: {
+    width: 50,
+    height: 50,
+  },
+  recipeTitle: {
+    fontSize: 16,
+    padding: 10,
+    flex: 1
   }
 });
 
--- app/reducers/recipes/recipes.actions.js
+++ app/reducers/recipes/recipes.actions.js
@@ -5,8 +5,6 @@ const { GET_RECIPES, SET_RECIPES } = require("../../lib/constants").default;
 export default function getRecipes() {
   return (dispatch) => {
     dispatch({ type: GET_RECIPES });
-    console.log(config.ROOT_URL + '/recipes.json');
-
     fetch(config.ROOT_URL + '/recipes.json')
       .then(response => response.json())
       .then(recipes => {
